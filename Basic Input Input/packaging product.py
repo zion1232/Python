ef42432fc42cc8aee2770147841fcd85f3316b6e
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-06 21:34:18 +0200

    Update packaging product.py

--- Basic Input Input/packaging product.py
+++ Basic Input Input/packaging product.py
@@ -7,8 +7,12 @@ Box_width=float(input("enter Box_width"))
 Box_depth=float(input("enter Box_depth"))
 Box_height=float(input("enter Box_height"))
 
-Box_size =  (Box_width*Box_height + Box_width*Box_depth + Box_height*Box_depth) * 2 * EXTRA_SIZE;
+
+
+#You have to double it twice because it's a box with 6 corners
+Box_size =  (Box_width*Box_height + Box_width*Box_depth + Box_height*Box_depth) * 2 * EXTRA_SIZE;  
 total_price = price + Box_size * PRICE_FOR_PAPER;
 
 print("You need to pay " , total_price , " NIS");
 
+#Calculate the price of a packaging product box and consider bottles including taxes
