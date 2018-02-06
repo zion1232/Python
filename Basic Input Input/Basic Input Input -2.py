350b8155e12494fc302e555d046503353e5b0aad
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-06 13:40:43 +0200

    Create Basic Input Input -2.py

---
+++ Basic Input Input/Basic Input Input -2.py
@@ -0,0 +1,7 @@
+start=float(10.20)
+kilometers=float(1.30)
+bag=int(2)
+k=float(input("How far you have to travel in kilometers "))
+b=int(input("How many bags do you have? "))
+total=float(k*kilometers+b*bag+start)
+print("For this trip you have to pay",float(total))
