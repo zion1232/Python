077e85d55f11c086df3ebee82efe194dcc457a28
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-06 21:33:12 +0200

    Create packaging product.py

---
+++ Basic Input Input/packaging product.py
@@ -0,0 +1,14 @@
+PRICE_FOR_PAPER = 5;
+taxes =1.18;
+EXTRA_SIZE= 1.12;
+
+price=float(input("enter price"))
+Box_width=float(input("enter Box_width"))
+Box_depth=float(input("enter Box_depth"))
+Box_height=float(input("enter Box_height"))
+
+Box_size =  (Box_width*Box_height + Box_width*Box_depth + Box_height*Box_depth) * 2 * EXTRA_SIZE;
+total_price = price + Box_size * PRICE_FOR_PAPER;
+
+print("You need to pay " , total_price , " NIS");
+
