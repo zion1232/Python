347314986cdd8c44c4eee19fedebc5f204a6daa8
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-06 12:11:10 +0200

    Update Basic Input Input - 1.py

--- Basic Input Input/Basic Input Input - 1.py
+++ Basic Input Input/Basic Input Input - 1.py
@@ -1,7 +1,8 @@
-print "hello ,Tickets cost NIS 35"
-a=input("how mach tickets you buy")
-print "you by " ,a," tickets"  ',' "You have to pay nis " ,a*35
+print ("hello ,Tickets cost NIS 35")
+a=int(input("how mach tickets you buy"))
+print ("you by " ,a," tickets "  ',' "You have to pay nis ",a*35)
 
     
 
 
+	
