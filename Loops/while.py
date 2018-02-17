89cc9bd2ade2b0a35331b6b62ae12b1ee0127930
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-17 10:18:15 +0200

    Create while.py

---
+++ Loops/while.py
@@ -0,0 +1,12 @@
+
+while True :
+
+ a =int(input("enter number 1 - "))
+ b =int(input("enter number 2 - "))
+ c =int(input("enter number 3 - "))		
+ d = a + b + c;
+ print("sum = ",d);
+ k =int(input("Continue Pressing A Positive Number to finish press negative number "))
+ if k<0:
+    break
+
