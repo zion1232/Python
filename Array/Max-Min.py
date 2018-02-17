26bb43e1e45d9f58782474c33679a4e964679492
Zion Davidov <30926908+zion1232@users.noreply.github.com>
2018-02-17 10:36:51 +0200

    Update Max-Min.py

--- Array/Max-Min.py
+++ Array/Max-Min.py
@@ -1,1 +1,17 @@
+max=0
+arr=[]
+for i in range(5):
+    arr.append(int(input("add number:")))
+if arr[i]>arr[max]:
+    max=i
+print("The max is at index " ,max ,"  and its value is ",arr[max])
+
+
+min=0
+arr=[]
+for i in range(5):
+    arr.append(int(input("add number:")))
+if arr[i]<arr[min]:
+    min=i
+print("The min is at index " ,min ,"  and its value is ",arr[min])
 
